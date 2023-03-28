9801ce58d2000ae81d9ce87992766cde20864b2f
championgirl <66965981+championgirl@users.noreply.github.com>
2023-03-27 18:24:16 -0700

    ArrayCheatSheetWIP

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -3,18 +3,21 @@ package arrays;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String [] arr = {"String1", "String2", "String3", "String4", "String5"};
 		//2. print the third element in the array
-		
+		System.out.println(arr[2]);
 		//3. set the third element to a different value
-		
+		arr[2] = "different string";
 		//4. print the third element again
-		
+		System.out.println(arr[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 		
 		//6. make an array of 50 integers
+		int []arr2 = new int[50];
 		
 		//7. use a for loop to make every value of the integer array a random number
 		
